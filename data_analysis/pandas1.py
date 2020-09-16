215b56d4eeabbbc7e292812a277f4df9d521b4fc
Sergio <salanitri@gmail.com>
2020-09-16 01:08:49 -0300

    primeslag usando Wagtail

---
+++ data_analysis/pandas1.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from bokeh.plotting import figure, output_file, show
+from bokeh.models import ColumnDataSource
+from bokeh.models.tools import HoverTool
+
+output_file('columndatasource_example.html')
+
+df = pd.read_csv('thor_wwii.csv')
+sample = df.sample(50)
+source = ColumnDataSource(sample)
+
+p = figure()
+p.circle(x='TOTAL_TONS', y='AC_ATTACKING',
+         source=source,
+         size=10, color='green')
+
+p.title.text = 'Attacking Aircraft and Munitions Dropped'
+p.xaxis.axis_label = 'Tons of Munitions Dropped'
+p.yaxis.axis_label = 'Number of Attacking Aircraft'
+
+hover = HoverTool()
+hover.tooltips=[
+    ('Attack Date', '@MSNDATE'),
+    ('Attacking Aircraft', '@AC_ATTACKING'),
+    ('Tons of Munitions', '@TOTAL_TONS'),
+    ('Type of Aircraft', '@AIRCRAFT_NAME')
+]
+
+p.add_tools(hover)
+
+show(p)
+
+
--- primes_api/primes_api/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from primes_api.models import Plist
-
-
-# primes_api
-admin.site.register(Plist)
